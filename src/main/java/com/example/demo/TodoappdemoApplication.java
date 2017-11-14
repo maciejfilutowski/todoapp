9bdd90676c898b277cfcac762277b60fd605b2b9
Maciej Filutowski <Maciej.Filutowski@gmail.com>
2017-11-14 09:32:33 +0100

    Project init

---
+++ src/main/java/com/example/demo/TodoappdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappdemoApplication.class, args);
+	}
+}
