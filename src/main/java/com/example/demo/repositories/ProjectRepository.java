5e7a429a7f33701905105e1b227b7916c1ba1b83
Maciej Filutowski <Maciej.Filutowski@gmail.com>
2017-11-14 11:04:19 +0100

    Created findByDescription method on projects

--- src/main/java/com/example/demo/repositories/ProjectRepository.java
+++ src/main/java/com/example/demo/repositories/ProjectRepository.java
@@ -3,8 +3,13 @@ package com.example.demo.repositories;
 import com.example.demo.model.Project;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by filutma1 on 2017-11-14.
  */
 public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+    Optional<Project> findByDescription(String description);
+
 }
